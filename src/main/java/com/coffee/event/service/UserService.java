65a6233b5312aebcd4b8f5fe4752ba022456f035
Subhra Goswami <subhra.goswami@neosofttech.com>
2024-09-05 17:30:22 +0530

    csv

--- src/main/java/com/coffee/event/service/UserService.java
+++ src/main/java/com/coffee/event/service/UserService.java
@@ -3,12 +3,14 @@ package com.coffee.event.service;
 import com.coffee.event.dao.UserDao;
 import com.coffee.event.entity.User;
 import com.coffee.event.exceptions.CoffeeException;
+import com.coffee.event.util.CsvUtil;
 import com.coffee.event.util.ErrorConstants;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.coffee.event.model.request.UserRequest;
 import com.coffee.event.model.response.ResponseDto;
 import com.coffee.event.model.response.UserResponse;
 import com.coffee.event.validator.UserValidator;
+import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.domain.Page;
@@ -16,6 +18,7 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
 import java.text.MessageFormat;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.UUID;
 import java.util.stream.Collectors;
@@ -27,6 +30,7 @@ public class UserService {
     private final UserDao userDao;
     private final ObjectMapper mapper;
     private final UserValidator userValidator;
+    private final CsvUtil csvUtil;
     public ResponseDto<String> saveUser(UserRequest userRequest){
         userValidator.requestValidator(userRequest);
         User user = mapper.convertValue(userRequest, User.class);
@@ -55,6 +59,18 @@ public class UserService {
                 .status(0)
                 .data(userResponseList)
                 .total(userResponseList.stream().count())
+                .build();
+    }
+
+    public ResponseDto<String> downloadCsv(HttpServletResponse response){
+        String headers = "User Id, User Email, User first name, user last name, event Id, email received at, created at, updated at";
+        List<String> csvContent = new ArrayList<>();
+        List<User> users = userDao.getAllUsers();
+        csvUtil.organizeCsv(csvContent, users);
+        csvUtil.downloadCSVFile(headers, csvContent, response, "User", "csv");
+        return ResponseDto.<String>builder()
+                .status(0)
+                .data(List.of("downloaded successfullly"))
                 .build();
     }
 }
