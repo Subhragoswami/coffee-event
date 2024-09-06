8f1eab38d310693e31989d69ab2824d6e8f25448
Subhra Goswami <subhra.goswami@neosofttech.com>
2024-09-06 19:59:51 +0530

    added eventListener

--- src/main/java/com/coffee/event/service/UserService.java
+++ src/main/java/com/coffee/event/service/UserService.java
@@ -2,8 +2,8 @@ package com.coffee.event.service;
 
 import com.coffee.event.dao.UserDao;
 import com.coffee.event.entity.User;
+import com.coffee.event.event.SendEmailEvent;
 import com.coffee.event.exceptions.CoffeeException;
-import com.coffee.event.util.AppConstants;
 import com.coffee.event.util.CsvUtil;
 import com.coffee.event.util.ErrorConstants;
 import com.fasterxml.jackson.databind.ObjectMapper;
@@ -11,20 +11,15 @@ import com.coffee.event.model.request.UserRequest;
 import com.coffee.event.model.response.ResponseDto;
 import com.coffee.event.model.response.UserResponse;
 import com.coffee.event.validator.UserValidator;
-import jakarta.mail.MessagingException;
-import jakarta.mail.internet.MimeMessage;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
-import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
-import org.springframework.mail.javamail.MimeMessageHelper;
-import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
 import org.thymeleaf.TemplateEngine;
-import org.thymeleaf.context.Context;
 
 import java.text.MessageFormat;
 import java.time.LocalDateTime;
@@ -33,7 +28,6 @@ import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 import java.util.UUID;
-import java.util.concurrent.CompletableFuture;
 import java.util.stream.Collectors;
 
 @Service
@@ -46,44 +40,18 @@ public class UserService {
     private final CsvUtil csvUtil;
     private final JavaMailSender mailSender;
     private final TemplateEngine templateEngine;
-    public ResponseDto<String> saveUser(UserRequest userRequest){
+    private final ApplicationEventPublisher eventPublisher; // Add ApplicationEventPublisher
+
+    public ResponseDto<String> saveUser(UserRequest userRequest) {
         userValidator.requestValidator(userRequest);
         User user = mapper.convertValue(userRequest, User.class);
-        sendSimpleEmail(user);
+        userDao.saveUser(user);
+        eventPublisher.publishEvent(new SendEmailEvent(this, user));
+
         return ResponseDto.<String>builder()
                 .status(0)
                 .data(List.of("user registered"))
                 .build();
-    }
-
-    @Async
-    public CompletableFuture<Void> sendSimpleEmail(User user) {
-        MimeMessage mimeMessage = mailSender.createMimeMessage();
-
-        try {
-            MimeMessageHelper helper = new MimeMessageHelper(mimeMessage, true, "UTF-8");
-            helper.setTo(user.getUserEmail());
-            helper.setSubject("Event Registration");
-
-            Context context = new Context();
-            context.setVariable("user", user);
-
-            String htmlContent = templateEngine.process("event-registration-email", context);
-            helper.setText(htmlContent, true);
-
-            helper.setFrom("your-email@gmail.com");
-
-            mailSender.send(mimeMessage);
-
-            LocalDateTime now = LocalDateTime.now();
-            user.setEmailReceivedAt(convertToDateViaInstant(now));
-            userDao.saveUser(user);
-        } catch (MessagingException e) {
-            log.error("Error sending email: {}", e.getMessage());
-            throw new CoffeeException(ErrorConstants.MAIL_SEND_ERROR_CODE, ErrorConstants.MAIL_SEND_ERROR_MESSAGE);
-        }
-
-        return CompletableFuture.completedFuture(null);
     }
 
     public ResponseDto<UserResponse> getById(UUID id){
